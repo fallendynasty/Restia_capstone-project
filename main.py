d7283e5600bb6d56b262f9ed763f28e8341f4119
PEH SIEW YU SIEW <86403209+pehhsy@users.noreply.github.com>
2022-04-18 05:45:29 +0000

    merge main

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+import database_functions
+app = Flask(__name__)
+
+@app.route('/') #Splash page (Welcome page)
+def index():
+    return render_template('index.html') 
+
+#form pages
+@app.route('/view/<form>')
+def view(form):
+    '''
+    views the form of specified type
+    '''
+    if form == 'club':
+        return render_template('view_club.html')
+        
+    if form == 'activity':
+        return render_template('view_activity.html')
+
+    if form == 'class':
+        return render_template('view_class.html')
+
+    else:
+        return render_template('view_student.html')
+        
+@app.route('/add/<form>',  methods=['GET', 'POST'])
+def add(form):
+    '''
+    goes to form for adding, post to send data in the form
+    '''
+    if form == 'club':
+        return render_template('add_club.html')
+
+    else:
+        return render_template('add_activity.html')
+
+        
+@app.route('/edit/<form>',  methods=['GET', 'POST'])
+def edit(form):
+    '''
+    goes to form for editing, post request so that changes can be sent to database
+    '''
+    if form == 'student-club_membership':
+        return render_template('edit_student-club_membership.html')
+        
+    else:
+        return render_template('edit_student-activity_participation.html')
+        
+app.run('0.0.0.0')
+#profile page to be implemented
+# @app.route('/<int:student_id>/profile')  
+# def view_profile(student_id):
+#     breakpoint()
+#     records = dict(request.form) #contain stuff needed
+#     if 'admin' in request.args: #admin page
+#         return render_template('admin_profile.html')
+#     return render_template('student_profile.html') 
+
+
+
